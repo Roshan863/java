2b7588deb1640fc0c05d6aa28d3daf6fe961bda2
Roshan863 <amk1003560@student.hamk.fi>
2024-11-01 15:32:43 +0200

    declared age 3 as 4

--- src/App.java
+++ src/App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        int age=3 ;
+        int age=4 ;
         if(age>0 && age<20){
             System.out.println(age +". You are underage to take License.");
          } else{
